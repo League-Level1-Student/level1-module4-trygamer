de8b6710898021eb67ca42159706328ae984f167
trygamer <league@WTS10.attlocal.net>
2018-08-03 11:50:20 -0700

    done

--- src/PracticeCode.java
+++ src/PracticeCode.java
@@ -1,13 +1,17 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class PracticeCode {
+public class PracticeCode implements ActionListener{
 JFrame f =new JFrame();
 JPanel p = new JPanel();
-JTextField tf = new JTextField();
+JTextField tf = new JTextField(26);
 JLabel l = new JLabel();
 JButton b = new JButton();
 JButton b1 = new JButton();
@@ -22,14 +26,39 @@ JButton b1 = new JButton();
 	f.setTitle("Riddle");
 	p.add(b);
 	b.setText("enter");
+	b.addActionListener(this);
 	p.add(b1);
 	b1.setText("hint");
 	p.add(tf);
 	p.add(l);
+	l.setText("What do u call something that is brown, white, and read all over. ");
+	b1.addActionListener(this);
+	
 	f.pack();
 		
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==b1) {
+		JOptionPane.showMessageDialog(null, "Where do we find the most words?");
+		}
+		else if(e.getSource()==b) {
+			if(tf.getText().equals("a book")) {
+				
+				JOptionPane.showMessageDialog(null, "correct");
+				
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "incorrect");
+			}
+			
+		}
+	}
 	
+	
+	                         
 	
 	
 	
